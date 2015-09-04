e8098b439efbafbc1cdb552f00f87d6da3dad121
Kleer LA <kleer@kleer-sandbox.(none)>
2015-09-04 17:20:52 -0300

    fnnn

--- spec/pelea_spec.rb
+++ spec/pelea_spec.rb
@@ -99,7 +99,7 @@ describe Pelea do
 	pelea.player_1_energy.should == 100
 	end
 
-<<<<<<< HEAD
+
 	it "Cuando ingreso a la app y el jugador 1 ataca, el usuario dos selecciona Energia 2 y luego Recargar 2 para recargar su vida" do
 	#Arrange
 	pelea = Pelea.new
@@ -111,6 +111,5 @@ describe Pelea do
 	pelea.player_2_score.should == 95
 	end
 
-=======
->>>>>>> 930a7d70a4fde3d1dbe87ca78a48e16166df698d
+
 end 
